b01114e86a3d380aa5f5a66eda22865af739102e
bekirytm <bekir.yetim.06@gmail.com>
2020-03-06 02:43:20 +0300

    ikinci commit

--- ui.js
+++ ui.js
@@ -19,6 +19,11 @@ class UI{
 
 //User Infoları Gösterme
     showUserInfo(user){
+
+        var t = user.updated_at;
+        var tt = t.slice(0,10);
+
+
         this.profileDiv.innerHTML = `
         <div class="card card-body mb-3">
                     <div class="row">
@@ -43,18 +48,21 @@ class UI{
                             <hr>
                             <li class="list-group">
                                 <li class="list-group-item borderzero">
-                                    <img src="images/company.png" width="30px"> <span id="company">${user.company}</span>
+                                    <img src="images/company.png" width="30px">&nbsp; <span id="company">${user.company}</span>
                                     
                                 </li>
                                 <li class="list-group-item borderzero">
-                                    <img src="images/location.png" width="30px"> <span id = "location">${user.location}</a>
+                                    <img src="images/location.png" width="30px">&nbsp; <span id = "location">${user.location}</a>
                                     
                                 </li>
                                 <li class="list-group-item borderzero">
-                                    <img src="images/mail.png" width="30px"> <span id="mail">${user.email}</span>
+                                    <img src="images/mail.png" title="Mail" width="30px">&nbsp; <span id="mail">${user.email}</span>
                                     
                                 </li>
-                                
+                                <li class="list-group-item borderzero">
+                                    <img src="images/calendar.png" title="Son güncelleme tarihi" width="30px">&nbsp; <span id="mail">${tt}</span>
+                                    
+                                </li>
                             </div>
                                
                             
@@ -75,19 +83,22 @@ class UI{
             this.repoDiv.innerHTML += `
             <div class="mb-2 card-body">
             <div class="row">
-                <div class="col-md-2">
+                <div class="col-md-3">
                 <span></span> 
                 <a href="${repo.html_url}" target = "_blank" id = "repoName">${repo.name}</a>
                 </div>
                 <div class="col-md-6">
                     <button class="btn btn-secondary">
-                        Starlar  <span class="badge badge-light" id="repoStar">${repo.stargazers_count}</span>
+                        Starlar :  <span class="badge badge-light" id="repoStar">${repo.stargazers_count}</span>
                     </button>
-
+                    &nbsp;&nbsp;
                     <button class="btn btn-info">
-                        Forklar  <span class="badge badge-light" id ="repoFork">${repo.forks_count}</span>
+                        Forklar :   <span class="badge badge-light" id ="repoFork">${repo.forks_count}</span>
                     </button>
-            
+                    &nbsp;&nbsp;
+                    <button class="btn btn-info">
+                        İzlenme :   <span class="badge badge-light" id ="repoFork">${repo.watchers}</span>
+                    </button>            
                 </div>
         </div>
 
